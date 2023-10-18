ab80de4129bd95ca83fa1c119b484404e469ebed
Sifiso-Nhlapo <146161188+Sifiso-Nhlapo@users.noreply.github.com>
2023-10-18 05:21:28 +0200

    Create working_script.sh

---
+++ working_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "CHECK IF NGINX IS INSTALLED ON THE WEB SERVER"
+systemctl is-active nginx
+
+echo "IF NGINX IS INSTALLED, CHECK IF NGINX IS RUNNING IF NOT THEN START IT"
+apt-get update
+apt-get -y install nginx
+systemctl start nginx
+
+echo "CREATE A DIRECTORY CALLED LABS"
+mkdir /var/www/html/labs
+ls -ld /var/www/html/labs
+
+echo "CHANGE OWNERSHIP OF DIRECTORY TO CURRENT USER"
+chown sifiso:root /var/www/html/labs
+
+echo "CREATE A SYMBOLIC LINK CALLED HTML UNDER HOME DIRECTORY"
+ln -s /var/www/html/labs html
+ls -l
+
+echo "CREATE AN INDEX.html FILE"
+cd html
+touch index.html
+
+echo "ADDING INFORMATION TO THE INEDX.HTML FILE"
+nano index.html
