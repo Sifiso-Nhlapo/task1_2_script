2c5c07b7e1c56f4d480b87c4bb73cc2875f6ced9
Sifiso-Nhlapo <146161188+Sifiso-Nhlapo@users.noreply.github.com>
2023-10-18 05:34:50 +0200

    Create mySQL.sh

---
+++ mySQL.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Access MySQL using the root user"
+mysql -u root -p
+
+echo "Create the student_records database"
+CREATE DATABASE student_records;
+
+echo "Use the student_records database"
+USE student_records;
+
+echo "Create the students table"
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    course VARCHAR(255) NOT NULL
+);
+
+echo "Insert sample data into the students table"
+INSERT INTO students (name, age, gender, course) VALUES
+('sifiso', 22, 'Male', 'Computer Systems'),
+('keletso', 22, 'Male', 'Computer Systems');
